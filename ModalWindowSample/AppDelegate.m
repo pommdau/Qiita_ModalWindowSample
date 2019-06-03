80aaa9e31e0c78f377e9c2225e5860a7f3ebf7ae
hikeuchi <hikeuchi@morisawa.co.jp>
2019-06-03 13:40:51 +0900

    AppDelegateに書いていた処理を新規のコントローラへ移行

--- ModalWindowSample/AppDelegate.m
+++ ModalWindowSample/AppDelegate.m
@@ -7,69 +7,25 @@
 //
 
 #import "AppDelegate.h"
-#import "SheetWindowController.h"
-#import "ModalWindowController.h"
+#import "MainWindowController.h"
 
 @interface AppDelegate ()
-@property (weak) IBOutlet NSTextField *resultLabel;
-@property SheetWindowController *sheetWindowController;
-@property ModalWindowController *modalWindowController;
 @property (weak) IBOutlet NSWindow *window;
+@property (strong) MainWindowController *mainWindowController;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    _mainWindowController = [[MainWindowController alloc] initWithWindowNibName: MainWindowController.className];
+    [_mainWindowController showWindow:self];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
-}
-
-/**
- @brief シートを開く
- */
-- (IBAction)openSheet:(id)sender {
-    SheetWindowController *sheetWindowController = [[SheetWindowController alloc] init];
-    _sheetWindowController = sheetWindowController;
-    
-    [self.window beginSheet:_sheetWindowController.window  completionHandler:^(NSModalResponse returnCode) {
-        
-        NSMutableString *resultMessage = [NSMutableString stringWithString: @"SheetResponse: "];
-        
-        switch (returnCode) {
-            case NSModalResponseOK:
-                [resultMessage appendString:@"OK"];
-                break;
-            case NSModalResponseCancel:
-                [resultMessage appendString:@"Cancel"];
-                break;
-            default:
-                break;
-        }
-        
-        self->_resultLabel.stringValue = resultMessage;
-    }];
-}
-
-/**
- @brief モーダルウィンドウを開く
- */
-- (IBAction)openModalWindow:(id)sender {
-    ModalWindowController *modalWindowController = [[ModalWindowController alloc] init];
-    _modalWindowController = modalWindowController;
-    
-    NSModalResponse result = [NSApp runModalForWindow:_modalWindowController.window];
-    NSMutableString *resultMessage = [NSMutableString stringWithString: @"ModalWindowResponse: "];
-    if (result == NSModalResponseOK) {
-        [resultMessage appendString:@"OK"];
-        
-    } else if (result == NSModalResponseCancel) {
-         [resultMessage appendString:@"Cancel"];
-    }
-    _resultLabel.stringValue = resultMessage;
 }
 
 @end
---
+++ ModalWindowSample/MainWindowController.h
@@ -0,0 +1,17 @@
+//
+//  MainWindowController.h
+//  ModalWindowSample
+//
+//  Created by HIROKI IKEUCHI on 2019/06/03.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
